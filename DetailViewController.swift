7138dd5e1aa073724070e4573a77b4fbb56c26a5
SUZUKIyuka <suzukiyuka1218@gmail.com>
2015-06-05 14:52:47 +0900

    first commit

---
+++ DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  iTunesAPISample
+//
+//  Created by nagata on 6/5/15.
+//  Copyright (c) 2015 nagata. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: AVPlayerViewController {
+   
+}
